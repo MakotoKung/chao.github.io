4cd10c06c8b9aa7976b0f31ce5eca41954d7932a
Shifuro <89166876+MakotoKung@users.noreply.github.com>
2024-03-01 14:45:42 +0700

    chaochao website and game

---
+++ test.js
@@ -0,0 +1,12 @@
+function calculate(){
+    var presure = document.getElementById('p');
+    var volume = document.getElementById('v');
+    
+    var ds = presure.value * volume.value ;
+    var text = document.createElement('a');
+    alert("ค่าคงที่แก๊ส = " + ds);
+    
+
+    
+    
+}
